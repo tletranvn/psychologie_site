8ab32c9cd4b48384932cb6c451c5d50ae6151f82
tletranvn <tletranvn@gmail.com>
2025-04-10 07:55:52 +0200

    page Contact

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,6 +4,7 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/contact", "Contact", "/pages/contact.html"),
+    new Route("/apropos", "A Propos", "/pages/apropos.html"),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Psychologie";
